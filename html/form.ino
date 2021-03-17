a87dd59cd0ead8c94ff4b3e61b910f4861083b81
MuhammadOwaisQadri <72045686+MuhammadOwaisQadri@users.noreply.github.com>
2021-03-17 17:04:46 +0500

    Add files via upload

---
+++ html/form.ino
@@ -0,0 +1,27 @@
+client.println("<!DOCTYPE html>");
+client.println("<html lang="en">");
+client.println("<head>");
+    client.println("<meta charset="UTF-8">");
+    client.println("<meta http-equiv="X-UA-Compatible" content="IE=edge">");
+    client.println("<meta name="viewport" content="width=device-width, initial-scale=1.0">");
+    client.println("<title>");My Pageclient.println("</title>");
+client.println("</head>");
+client.println("<body>");
+    client.println("<h1>");Headingclient.println("</h1>");
+    client.println("<p>");Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nisi, unde culpa nam ipsum laboriosam
+        inventore ducimus. Eius eum minima nobis quaerat assumenda, dolorum qui voluptatem, vel dolores minus
+        fuga.client.println("</p>");
+       
+        client.println("<img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/NEDUET_logo.svg/1200px-NEDUET_logo.svg.png" sizes="200px" width="200px" alt="">");
+        client.println("<form action="">");
+
+            client.println("<label>");Nameclient.println("</label>");
+            client.println("<input type="text">");
+            client.println("<br>");client.println("<br>");
+            client.println("<label>");IDclient.println("</label>");
+            client.println("<input type="text">");
+        client.println("</form>");
+
+
+client.println("</body>");
+client.println("</html>");"
